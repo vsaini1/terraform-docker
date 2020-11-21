f36682f7630ced417e00c3d30884ad0b94679ebc
vsaini1 <74753490+vsaini1@users.noreply.github.com>
2020-11-20 23:49:32 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+##main.tf
+terraform {
+  required_providers {
+    docker = {
+      source = "terraform-providers/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "docker-test1"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
